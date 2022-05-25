cef4b4dd08f57c27d01887ffa860b308c8f15f03
uriassm <uriassmira@gmail.com>
2022-05-25 16:12:02 -0700

    Update letters_guessed.py with comments

--- jumper/game/letters_guessed.py
+++ jumper/game/letters_guessed.py
@@ -9,8 +9,16 @@ class LettersGuessed:
 
 
     def get_guessed_letter(self):
+        '''Gets the most recent letter that was guessed.
+        
+        Args:
+            self(LettersGuessed): an instance of lettersguessed.'''
         return self._guessed_letter
 
     def update_guessed_letter(self, letter):
+        '''Updates what the most recent letter was.
+        
+        Args:
+            self(LettersGuessed): an instance of lettersguessed.
+            letter: the letter the user input in the terminal. '''
         self._guessed_letter = letter
-        #self._past_letters.append(letter)
