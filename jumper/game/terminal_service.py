582359229594b74d0d5c98a90d673ab1d3598013
Fulton Allred <fultonallred@gmail.com>
2022-05-25 14:34:40 -0700

    Started the terminal service file.

---
+++ jumper/game/terminal_service.py
@@ -0,0 +1,18 @@
+class terminal_service:
+    """Will get inputs and display outputs.
+    
+    Attributes:
+    """
+
+
+    def __init__(self):
+        """Construct a new instance of terminal service."""
+
+        
+
+    def write_text(self, to_print):
+        """"""
+        print(to_print)
+
+    def read_text(self):
+        """"""
