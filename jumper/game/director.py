fbc7ad67b455c6d69518c72b1bc0838809b73bdc
Fulton Allred <fultonallred@gmail.com>
2022-05-25 18:02:03 -0700

    Removed references to terminal_service in Director, added comment, unified formatting, added white space

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,7 +1,6 @@
 from game.word_selection import WordSelection
 from game.parachute_drawing import ParachuteDrawing
 from game.letters_guessed import LettersGuessed
-from game.terminal_service import TerminalService
 
 class Director:
     '''A person who directs the game.
@@ -12,7 +11,6 @@ class Director:
         _word_selector: The one who selects the word and compares the guessed letters
         _guessed_letters: the letters the user will input and where it is maintained
         _parachute_drawing: drawing that determines how long the game lasts
-        _terminal_service: will get inputs and display outputs
         '''
 
     def __init__(self):
@@ -24,15 +22,14 @@ class Director:
         self._word_selector = WordSelection()
         self._letters_guessed = LettersGuessed()
         self._parachute_drawing = ParachuteDrawing()
-        self._terminal_service = TerminalService()
         self._is_playing = True
 
     def start_game(self):
-        """Starts the game by running the main game loop.
+        '''Starts the game by running the main game loop.
         
         Args:
             self (Director): an instance of Director.
-        """
+        '''
         while self._is_playing:
             self._do_outputs()
             self._do_inputs()
@@ -45,8 +42,12 @@ class Director:
         
         Args:
             self(Director): an instance of director'''
+
+        # Hidden word is the secret word displayed with blank spaces
+        # and letters that have been guessed.
         hidden_word = self._word_selector.get_hidden_word()
-        self._terminal_service.write_text(hidden_word)
+        print(hidden_word)
+
         self._parachute_drawing.create_drawing()
 
     def _do_inputs(self):
@@ -55,7 +56,8 @@ class Director:
         
         Args:
             self(DIrector): and instance of director'''
-        new_letter = self._terminal_service.read_text("Guess a letter [a-z]: ")
+
+        new_letter = input("Guess a letter [a-z]: ")
         self._letters_guessed.update_guessed_letter(new_letter)
 
     def _do_updates(self):
@@ -65,10 +67,13 @@ class Director:
         
         Args:
             self (Director): and instance of director.'''
+
         self._word_selector.compare_letters(self._letters_guessed)
         self._parachute_drawing.update_drawing(self._word_selector)
+
         if self._word_selector.word_found() == False:
             self._is_playing = False
+            
         if self._parachute_drawing.parachute_gone() == True:
             self._is_playing = False
 
